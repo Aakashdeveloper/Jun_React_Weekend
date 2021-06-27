3d38ae8e22ce2fe89edb762098bb1b0f9522e1f2
aakashdeveloper <ahanda205@gmail.com>
2021-06-27 09:15:11 +0530

    stateprops

--- firstapp/src/component/Header.js
+++ firstapp/src/component/Header.js
@@ -2,14 +2,31 @@ import React,{Fragment, Component} from 'react';
 import './Header.css';
 
 class Header extends Component {
+    constructor(){
+      super()
+
+      this.state={
+        title:'My React App',
+        userInput: 'User Text Here'
+      }
+    }
+
+    handleChange = (event) => {
+        // console.log(event.target.value)
+        this.setState({userInput:event.target.value})
+        // call property and pass data
+        this.props.userText(event.target.value)
+    }
+
     render(){
+        console.log(">>>>>inside render>>>>>")
         return(
             <Fragment>
                 <header>
-                    <div className="logo">React App</div>
+                    <div className="logo">{this.state.title}</div>
                 <center>
-                    <input/>
-                    <div style={{color:'white'}}>User Text Here</div>
+                    <input onChange={this.handleChange}/>
+                    <div style={{color:'white'}}>{this.state.userInput}</div>
                 </center>
                 <hr/>
                 </header>
--- firstapp/src/component/Home.js
+++ firstapp/src/component/Home.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './Header';
-import Footer from './Footer'
+import Footer from './Footer';
+import JSON from './db.json';
+import NewsDisplay from './NewsDisplay';
 
-const Home = () => {
-    return(
-        <React.Fragment>
-            <Header/>
-            <h1>Say Hi To React</h1>
-            <h2>React</h2>
-            <Footer/>
-        </React.Fragment>
-    )
+class Home extends Component {
+    constructor() {
+        super()
+
+        this.state={
+            news: JSON,
+            filtered: JSON
+        }
+    }
+
+    /*
+        var a = [3,4,5,7,6,3,2,8]
+        a.filter((data) => {return data>5})
+    */
+    filterNews = (userInput) => {
+        const output = this.state.news.filter((data) => {
+            return data.title.toLowerCase().indexOf(userInput.toLowerCase())>-1
+        })
+
+        this.setState({filtered:output})
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Header userText={(data) => {this.filterNews(data)}}/>
+                <NewsDisplay newsData={this.state.filtered}/>
+                <Footer year="2021" month="June"/>
+            </React.Fragment>
+        )
+    }
+    
 }
 
 export default Home;
